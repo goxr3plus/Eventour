b13432b3f010bc64430141ba6c76fb876415994d
Kentros Alexandros <crAlexcomp@gmail.com>
2019-12-12 18:24:18 +0200

    Dependencies Updates

--- src/App.js
+++ src/App.js
@@ -108,4 +108,5 @@ const query = ({ width }) => {
   return { breakpoint: 'xs' };
 };
 
-export default componentQueries(query)(App);
+// export default componentQueries(query)(App);
+export default App;
